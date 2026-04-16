49fed023bb54ba9b823e5e732e3f0696bb837b60
interlinked01 <sarah.mouzai01@gmail.com>
2026-04-16 02:17:49 +0200

    login update

--- login.php
+++ login.php
@@ -2,9 +2,9 @@
     include"db_config.php";
     $email =$_POST['email'];
     $password = $_POST['password'];
-    $sql="select*from user where email='$email' and password='$password'";
-    $result = mysqli_qurey($conn,$sql);
-    if(mysql_num_rows ($result)>0 ){
+    $sql="select*from user where email='$email' and pwd='$password'";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows ($result)>0 ){
         echo"login success";
     }
     else{
