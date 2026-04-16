6352acc3d2424ab7f43fedaac1d029323ed32c52
interlinked01 <sarah.mouzai01@gmail.com>
2026-04-16 02:26:23 +0200

    Fix SQL query to select from 'users' table

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     include"db_config.php";
     $email =$_POST['email'];
     $password = $_POST['password'];
-    $sql="select*from user where email='$email' and pwd='$password'";
+    $sql="select*from users where email='$email' and pwd='$password'";
     $result = mysqli_query($conn,$sql);
     if(mysqli_num_rows ($result)>0 ){
         echo"login success";
